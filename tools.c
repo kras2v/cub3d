9b29817ffabb7410849e181d7ef41dc28deb723a
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2025-06-24 21:12:56 +0200

    fix player movement

--- tools.c
+++ tools.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valeriia <valeriia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kvalerii <kvalerii@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 17:03:35 by valeriia          #+#    #+#             */
-/*   Updated: 2025/06/24 12:42:58 by valeriia         ###   ########.fr       */
+/*   Updated: 2025/06/24 21:09:37 by kvalerii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,6 +129,14 @@ double	find_distance_to_wall(
 		if (data->map[player_cell.y][player_cell.x] > 0)
 			hit = true;
 	}
+	t_fvector player_position_in_pixels;
+	player_position_in_pixels.x = data->player.position.x * CELL_SIZE + PLAYER_SIZE / 2;
+	player_position_in_pixels.y = data->player.position.y * CELL_SIZE + PLAYER_SIZE / 2;
+	t_fvector end_ray_point;
+	end_ray_point.x = player_cell.x * CELL_SIZE;
+	end_ray_point.y = player_cell.y * CELL_SIZE;
+	draw_line(data, player_position_in_pixels, end_ray_point, WHITE, true);
+
 	if (*side == VERTICAL)
 		return (initial_step->x - fixed_step.x);
 	return (initial_step->y - fixed_step.y);
@@ -148,38 +156,38 @@ void	get_start_and_end_draw_pixels(
 		*end_pixel = HEIGHT - 1;
 }
 
-void	make_it_color(
-	t_side side,
+void	draw_floor(
 	t_data *data,
-	t_coordinates player_cell,
-	int start_pixel,
 	int end_pixel,
+	t_colors color,
 	int x
 )
 {
-	t_colors color;
-	switch(data->map[player_cell.y][player_cell.x])
-	{
-	case 1:  color = RED;  break; //red
-	case 2:  color = GREEN;  break; //green
-	case 3:  color = BLUE;   break; //blue
-	case 4:  color = WHITE;  break; //white
-	default: color = YELLOW; break; //yellow
-	}
+	t_fvector	screen_bottom;
+	t_fvector	start_point;
 
-	//give x and y sides different brightness
-	if (side == 1) {color = color / 2;}
+	screen_bottom.x = x;
+	screen_bottom.y = HEIGHT - 1;
+	start_point.x = x;
+	start_point.y = end_pixel;
+	draw_line(data, screen_bottom, start_point, color, false);
+}
 
-	//draw the pixels of the stripe as a vertical line
-	t_fvector a;
-	a.x = x;
-	a.y = start_pixel;
+void	draw_celing(
+	t_data *data,
+	int start_pixel,
+	t_colors color,
+	int x
+)
+{
+	t_fvector	screen_top;
+	t_fvector	start_point;
 
-	t_fvector b;
-	b.x = x;
-	b.y = end_pixel;
-	//   printf("a(%f, %f) b(%f, %f)\n", a.x, a.y, b.x, b.y);
-	draw_line(data, a, b, color, 0);
+	screen_top.x = x;
+	screen_top.y = 0;
+	start_point.x = x;
+	start_point.y = start_pixel;
+	draw_line(data, screen_top, start_point, color, false);
 }
 
 void	draw_line_text(
@@ -218,6 +226,8 @@ void	draw_line_text(
 	//if(side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
 	y_increase_step = 1.0 * (double)data->texture->height / (double)line_height;
 	position_on_texture = (start_pixel - HEIGHT / 2 + line_height / 2) * y_increase_step;
+	draw_celing(data, start_pixel, LIGHT_BLUE, x);
+	draw_floor(data, end_pixel, GRAY, x);
 	while (start_pixel < end_pixel)
 	{
 		position_on_texture += y_increase_step;
@@ -233,8 +243,6 @@ void	draw_line_text(
 		offset = data->texture[text_num].image.addr + (texY * data->texture[text_num].image.line_length 
 			+ texture.x * data->texture[text_num].image.bits_per_pixel / 8);
 		color = *(unsigned int *)offset;
-		// if (text_num != 3)
-		// 	printf("%d\n", text_num);
 		my_mlx_pixel_put(&data->img, x, start_pixel, color);
 		start_pixel++;
 	}
@@ -266,179 +274,6 @@ void	dda(t_data *data)
 		x++;
 	}
 }
-
-// void	dda(t_data *data)
-// {
-// 	double	pos_pl_x; //
-// 	double	pos_pl_y; //
-// 	double	map_x; //
-// 	double	map_y; //
-// 	double	camera_x; //
-// 	double	side_dist_x;
-// 	double	side_dist_y;
-// 	double	delta_dist_x;
-// 	double	delta_dist_y;
-// 	int		step_x;
-// 	int		step_y;
-// 	int		hit;
-// 	t_side	side;
-// 	double	perp_wall_dist;
-// 	int		line_height;
-
-// 	int		x;
-
-// 	x = 0;
-// 	side = VERTICAL;
-// 	double theta_player = atan2(data->player.direction.y, data->player.direction.x);
-// 	double cos_theta_ray;
-// 	double sin_theta_ray;
-// 	double theta_ray;
-
-// 	double sin_cos[2][WIDTH];
-
-// 	x = 0;
-// 	while (x < WIDTH)
-// 	{
-// 		hit = 0;
-
-// 		pos_pl_x = (data->player.position.x / CELL_SIZE);
-// 		pos_pl_y = (data->player.position.y / CELL_SIZE);
-
-// 		map_x = floor(pos_pl_x);
-// 		map_y = floor(pos_pl_y);
-
-// 		cos_theta_ray = sin_cos[1][x];
-// 		sin_theta_ray = sin_cos[0][x];
-
-// 		delta_dist_x = fabs(1.0 / cos_theta_ray);
-// 		delta_dist_y = fabs(1.0 / sin_theta_ray);
-
-
-
-// 		if (sin_theta_ray < 0.0)
-// 		{
-// 			step_y = -1;
-// 			side_dist_y = (pos_pl_y - map_y) * delta_dist_y;
-// 		}
-// 		else
-// 		{
-// 			step_y = 1;
-// 			side_dist_y = (1.0 + map_y - pos_pl_y) * delta_dist_y;
-// 		}
-
-// 		while (hit == 0.0)
-// 		{
-// 			if (side_dist_x < side_dist_y)
-// 			{
-// 				side_dist_x += delta_dist_x;
-// 				map_x += step_x;
-// 				side = VERTICAL;
-// 			}
-// 			else
-// 			{
-// 				side_dist_y += delta_dist_y;
-// 				map_y += step_y;
-// 				side = HORIZONTAL;
-// 			}
-// 			if (data->map[(int)map_y][(int)map_x] > 0 && !is_direction(data->map[(int)map_y][(int)map_x]))
-// 				hit = 1;
-// 		}
-// 		if (side == VERTICAL)
-// 			perp_wall_dist = (side_dist_x - delta_dist_x);
-// 		else
-// 			perp_wall_dist = (side_dist_y - delta_dist_y);
-
-// 		t_fvector	new_point;
-// 		new_point.x = map_x * CELL_SIZE;
-// 		new_point.y = map_y * CELL_SIZE;
-// 		draw_line(data, data->player.position, new_point, 0xFFFFFF, 1);
-
-// 		double corrected_dist = perp_wall_dist /* cos(theta_ray - theta_player) */;
-// 		line_height = HEIGHT / corrected_dist;
-
-// 		// t_fvector	first;
-// 		// t_fvector	second;
-
-// 		// first.x = x;
-// 		// second.x = x;
-// 		// first.y = start;
-// 		// second.y = end;
-
-// 		// t_colors color;
-// 		// switch(data->map[map_y][map_x])
-// 		// {
-// 		// 	case 1:  color = RED;  break; //red
-// 		// 	case 2:  color = GREEN;  break; //green
-// 		// 	case 3:  color = BLUE;   break; //blue
-// 		// 	case 4:  color = YELLOW;  break; //white
-// 		// 	default: color = WHITE; break; //yellow
-// 		// }
-
-// 		// if (side == 1)
-// 		// 	color = color / 1.5;
-// 		// // draw_line(data, first, second, color, 0);
-// 		// while (first.y < second.y)
-// 		// { 
-// 		// 	put_image(data, first.x, first.y);
-// 		// 	first.y += data->texture->height;
-// 		// 	// printf("%f - %f\n", first.y, second.y);
-// 		// }
-
-// 		// int		tex_num = data->map[map_x][map_y] - 1; //1 subtracted from it so that texture 0 can be used!
-// 		double	wall; //where exactly the wall was hit
-// 		if (side == VERTICAL)
-// 		{
-// 			wall = pos_pl_x + perp_wall_dist * cos_theta_ray;
-// 		}
-// 		else
-// 		{
-// 			wall = pos_pl_y + perp_wall_dist * sin_theta_ray;
-// 		}
-// 		wall -= floor(wall);
-// 		int	tex_x = (int)(wall * (double)(data->texture->width)); //the pixel column (texX) of the texture you should draw.
-// 		//So you flip the texture column horizontally if the ray hits the wall from the opposite direction.
-// 		if ((side == VERTICAL && cos_theta_ray > 0) || (side == HORIZONTAL && sin_theta_ray < 0))
-// 		{
-// 			tex_x = data->texture->width - tex_x - 1;
-// 		}
-// 		double step = 1.0 * (double)data->texture->height / line_height;
-// 		int start =  HEIGHT / 2 - line_height / 2;
-// 		if (start < 0)
-// 			start = 0;
-// 		int end =  HEIGHT / 2 + line_height / 2;
-// 		if (end >= HEIGHT)
-// 		end = HEIGHT - 1;
-// 		// This centers the texture vertically on the wall slice.
-// 		double tex_pos = (start - HEIGHT / 2 + line_height / 2) * step;
-// 		int	y = start;
-// 		t_texture *texture;
-// 		texture = malloc(sizeof(t_texture));
-// 		texture->image.bits_per_pixel = 0;
-// 		texture->image.line_length = 0;
-// 		texture->image.addr = 0;
-// 		while (y < end)
-// 		{
-// 			//This helps prevent texture coordinate overflow (going out of bounds) by effectively performing a modulo operation, but it's faster because it's just a bitwise AND.
-// 			int tex_y = (int)tex_pos & (data->texture->height - 1);
-// 			tex_pos += step;
-// 			if (side == VERTICAL && cos_theta_ray > 0)
-// 				texture = &data->texture[1];
-// 			else if (side == VERTICAL && cos_theta_ray <= 0)
-// 				texture = &data->texture[3];
-// 			if (side == HORIZONTAL && sin_theta_ray > 0)
-// 				texture = &data->texture[0];
-// 			else if (side == HORIZONTAL && sin_theta_ray <= 0)
-// 				texture = &data->texture[2];
-// 			char *offset = texture->image.addr + (tex_y * texture->image.line_length + x * texture->image.bits_per_pixel / 8);
-// 			int color = *(unsigned int *)offset;
-// 			// if (y == start)
-// 			// 	printf("color %d and texture %s\n", color, data->texture->name);
-// 			my_mlx_pixel_put(&data->img, x, y, color);
-// 			y++;
-// 		}
-// 		x++;
-// 	}
-// }
 
 void	draw_horizontal(t_data *data, t_fvector a, t_fvector b, t_colors color, int second_window)
 {
@@ -530,8 +365,6 @@ void	draw_vertical(t_data *data, t_fvector a, t_fvector b, t_colors color, int s
 
 void	draw_line(t_data *data, t_fvector a, t_fvector b, t_colors color, int second_window)
 {
-	// a.x += PLAYER_SIZE / 2;
-	// a.y += PLAYER_SIZE / 2;
 	if (fabs(b.x - a.x) >= fabs(b.y - a.y))
 	{
 		draw_horizontal(data, a, b, color, second_window);
@@ -539,19 +372,5 @@ void	draw_line(t_data *data, t_fvector a, t_fvector b, t_colors color, int secon
 	else
 	{
 		draw_vertical(data, a, b, color, second_window);
-	}
-}
-
-#include <errno.h>
-#include <string.h>
-#include <string.h>
-#include <unistd.h>
-
-void	put_image(t_data *data, t_texture *texture, int x, int y)
-{
-	(void)y;
-	if (mlx_put_image_to_window(data->mlx, data->mlx_win, texture->image.ptr, x * texture->width, y))
-	{
-		printf("NO PUT IMAGE %s\n", strerror(errno));
 	}
 }
